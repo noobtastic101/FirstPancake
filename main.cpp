5c1dffcf880ca5cfcac22fcb922e7091d9647d61
Kyle Gullicksen <noobtastic101@gmail.com>
2017-09-13 10:54:38 -0700

    started on the tests

--- main.cpp
+++ main.cpp
@@ -1,13 +1,41 @@
 #include <iostream>
 #include <vector>
 
+#include "Processes.h"
+
 using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    runTestOne();
+}
 
 
-    vector<int> * myVec = new vector<int>();
+/*
+ *
+(a) add processes 5, 1, 8, 11 to q1. Display the content of q1
+(b) remove the process with the highest priority from q1 and display q1.
+(c) remove the process with the highest priority from q1 and display q1
+(d) Insert processes 3, 7, 2, 12, 9 to q1
+(e) One by one remove the process with the highest priority from the queue q1 and display it after each removal.
+ */
 
-    return 0;
+
+void runTestOne()
+{
+    Processes * processes = new Processes();
+
+    //Add 20 processes, with a READY state
+
+    ProcessControlBlock * currentBlock;
+
+    for(int count = 1; count < 21; count++)
+    {
+        currentBlock = new ProcessControlBlock();
+        currentBlock->setReady(true);
+
+        processes->insertProc(currentBlock);
+    }
+
+
+    delete processes;
 }
